c9f136d9a01a6b5ef2769a9569ba90b0df0c11a8
konstantin-moro <konstantinmorozovvv@gmail.com>
2019-10-27 15:22:21 +0300

    components examples

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <Navbar />
     <HelloWorld msg="Deadliner"/>
+    <ProgressBar />
   </div>
 </template>
 
 <script>
+import Navbar from "./components/Navbar"
 import HelloWorld from './components/HelloWorld.vue'
+import ProgressBar from "./components/ProgressBar"
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    ProgressBar,
+    Navbar
   }
 }
 </script>
---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <div v-for="bar in bars" :key="bar.label" class="row mb-1">
+            <div class="col-sm-2 ">{{ bar.variant }}:</div>
+            <div class="col-sm-10 pt-1">
+                <b-progress :value="bar.value" :variant="bar.variant" :key="bar.variant"></b-progress>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                bars: [
+                    { variant: 'success', value: 75 },
+                    { variant: 'info', value: 75 },
+                    { variant: 'warning', value: 75 },
+                    { variant: 'danger', value: 75 },
+                    { variant: 'primary', value: 75 },
+                    { variant: 'secondary', value: 75 },
+                    { variant: 'dark', value: 75 }
+                ],
+                timer: null
+            }
+        },
+        mounted() {
+            this.timer = setInterval(() => {
+                this.bars.forEach(bar => (bar.value = 25 + Math.random() * 75))
+            }, 2000)
+        },
+        beforeDestroy() {
+            clearInterval(this.timer)
+            this.timer = null
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
