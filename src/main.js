d27bab42f7ce2b7b0cbcb9b4f8be470728fdb699
konstantin-moro <konstantinmorozovvv@gmail.com>
2019-10-28 18:45:10 +0300

    router

--- src/main.js
+++ src/main.js
@@ -1,13 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 import BootstrapVue from 'bootstrap-vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import ProgressBar from "./components/ProgressBar"
+import Notifications from "./components/Notifications"
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: [
+    { path: '/', component: ProgressBar },
+    { path: '/notifications', component: Notifications}
+  ],
+  mode: 'history'
+});
 
 new Vue({
+  router,
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
+
+
