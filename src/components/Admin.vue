1512849ae2a98d0f1001fefe7f245c37f70c9a3c
konstantin-moro <konstantinmorozovvv@gmail.com>
2019-10-29 13:54:46 +0300

    admin

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -4,16 +4,25 @@
         <b-form-group id="input-group-3" label="Предмет" label-for="input-3">
             <b-form-select
                     id="input-3"
-                    v-model="form.subject"
-                    :options="subject"
+                    v-model="form.subjectName"
+                    :options="subjectName"
                     required
             ></b-form-select>
+        </b-form-group>
+
+        <b-form-group id="input-group-2" label="Название/номер ДЗ" label-for="input-2">
+            <b-form-input
+                    id="input-2"
+                    v-model="form.name"
+                    required
+                    placeholder="Название/номер ДЗ"
+            ></b-form-input>
         </b-form-group>
 
         <b-form @submit="onSubmit" @reset="onReset" v-if="show">
             <b-form-group
                     id="input-group-1"
-                    label="Домашнее задание"
+                    label="Опсиание"
                     label-for="input-1"
             >
                 <b-form-textarea
@@ -28,13 +37,22 @@
                 <b-form-input
                         id="date"
                         type='date'
-                        v-model="form.data"
+                        v-model="form.date"
                         required
                 ></b-form-input>
             </b-form-group>
 
-            <b-button type="submit" class="mr-1" variant="primary">Добавить</b-button>
-            <b-button type="reset" variant="danger">Отмена</b-button>
+<!--            <b-form-group id="input-group-2" label="Время дэдлайна" label-for="time">-->
+<!--                <b-form-input-->
+<!--                        id="time"-->
+<!--                        type='time'-->
+<!--                        v-model="dateFormat.time"-->
+<!--                        required-->
+<!--                ></b-form-input>-->
+<!--            </b-form-group>-->
+
+            <b-button type="submit" class="mr-1" variant="danger">Добавить</b-button>
+
         </b-form>
         <b-card class="mt-3" header="Form Data Result">
             <pre class="m-0">{{ form }}</pre>
@@ -46,12 +64,18 @@
     export default {
         data() {
             return {
+                // dateFormat: {
+                //     date: null,
+                //     time: null
+                // },
                 form: {
-                    subject: null,
+                    name: null,
+                    subjectName: null,
                     description: null,
-                    data: null
+                    dateTimeStart: new Date(),
+                    // dateTimeFinish: this.dateFormat.date + this.dateFormat.time
                 },
-                subject: [{ text: 'Предмет:', value: null }, 'C++', 'Unix/Python', 'Алгоритмы', 'Базы данных',
+                subjectName: [{ text: 'Предмет:', value: null }, 'C++', 'Unix/Python', 'Алгоритмы', 'Базы данных',
                     'Комбинаторика', 'Операционные системы', 'Основы программной инженерии',
                     'Функциональное программирование'],
                 show: true
--- src/components/Notifications.vue
+++ src/components/Notifications.vue
@@ -45,6 +45,24 @@
                         title: "Вопросы жизни и смерти",
                         desc: 'Попросить перенести дэдлайн по алгоритмам',
                         hidden: true
+                    },
+                    {
+                        title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+                        desc:
+                            "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                        hidden: true
+                    },
+                    {
+                        title: "Организационные моменты",
+                        desc: 'Отправить паспортные данные для коропоративной стипендии',
+                        hidden: true
+                    },
+                    {
+                        title: "Организационные моменты",
+                        desc: 'Отправить паспортные данные для коропоративной стипендии',
+                        style: "danger",
+                        textColor: "white",
+                        hidden: true
                     }
                 ]
             }
